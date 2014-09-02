808ce0f9065d2f08b310c579a5e6d05832cb907d
Taryn Carroll <tac217@lehigh.edu>
2014-09-02 23:45:12 +0000

    final WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Taryn Carroll
+//Homework number 1
+
+// first compile the program
+//     javac HelloWorld.java
+// run the program
+//     java HelloWorld//
+
+// Defining the class
+public class WelcomeClass{
+    
+    // add main method
+    public static void main(String[] args) {
+
+        // print the assignment out
+        System.out.println("  ------------");
+        System.out.println("  |  WELCOME |");
+        System.out.println("  ------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-T--A--C--2--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("This year I will be involved with the rowing team, residence hall association and admissions ambassadors ");
+
+    }
+}
